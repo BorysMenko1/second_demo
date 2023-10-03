07399965b99a1b8111dcce794e0a947bf80f60b5
BorysMenko1 <borismenko.s@gmail.com>
2023-10-03 18:41:19 +0300

    commpit

--- website/auth.py
+++ website/auth.py
@@ -64,3 +64,7 @@ def sign_up():
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
+
+@auth.route("/", methods=["GET"])
+def get_health():
+    return jsonify({"status": "ok"})
